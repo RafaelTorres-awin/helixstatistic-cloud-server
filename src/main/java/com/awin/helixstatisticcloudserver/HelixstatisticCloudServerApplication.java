703842521be14bb86664f797c19aac174e0ef4ba
Rafael Torres <rafaelmartins.torres@awin.com>
2018-11-12 14:52:23 +0100

    Initial commit

---
+++ src/main/java/com/awin/helixstatisticcloudserver/HelixstatisticCloudServerApplication.java
@@ -0,0 +1,12 @@
+package com.awin.helixstatisticcloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelixstatisticCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelixstatisticCloudServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/awin/helixstatisticcloudserver/HelixstatisticCloudServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.awin.helixstatisticcloudserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelixstatisticCloudServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
